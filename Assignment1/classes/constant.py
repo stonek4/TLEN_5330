ad9d47e0f89e8d78560907eba6419862803e998d
Kevin Stoner <stonek4rpi@gmail.com>
2016-09-23 10:20:34 -0400

    added server handling

--- Assignment1/classes/constant.py
+++ Assignment1/classes/constant.py
@@ -1,4 +1,5 @@
 class CONSTANT:
-    invalid_arguments = "Invalid Arguments (python <client.py> <ip address> <port number>)"
+    invalid_arguments = "Invalid Arguments: (python <client.py> <ip address> <port number>)"
+    invalid_port_number = "Invalid Arguments: port number must be larger than 5000"
     def __init__(self):
         return
--- Assignment1/server.py
+++ Assignment1/server.py
@@ -1,7 +1,14 @@
+import sys
+from classes.constant import CONSTANT
 from classes.listener import LISTENER
 
 def main():
+    if (len(sys.argv) != 2):
+        print CONSTANT.invalid_arguments
+        sys.exit(2)
+    elif (int(sys.argv[1]) <= 5000):
+        print CONSTANT.invalid_port_number
     listener = LISTENER(5000, 1024)
     listener.start()
 
-main();
+main()
